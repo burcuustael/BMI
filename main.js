e77d45ce19453719700f4f40deccb831435cdf30
burcuustael <burcustael@gmail.com>
2023-09-06 13:26:18 +0300

    BMI Calculator tamamlandı.

--- main.js
+++ main.js
@@ -12,8 +12,27 @@ function CalculateBMI(){
         document.getElementById('result-statu').innerText = "Under Weight";
         document.getElementById('result-statu').style.color='#2196f3';
     }
+    else if(result>=18.5 && result<25)
+    {
+        document.getElementById('result-statu').innerText = "Normal";
+        document.getElementById('result-statu').style.color='#43a047';
+    }
 
-    if()
+    else if(result>=25 && result<30)
+    {
+        document.getElementById('result-statu').innerText = "Over Weight";
+        document.getElementById('result-statu').style.color='#fb8c00';
+    }
+    else if(result>=30)
+    {
+        document.getElementById('result-statu').innerText = "Obese";
+        document.getElementById('result-statu').style.color='#c62828';
+    }
+}
 
-
+function Reset(){
+    document.getElementById('weight').value=0;
+    document.getElementById('height').value=0; 
+    document.getElementById('result').innerText = '';
+    document.getElementById('result-statu').innerText = '';
 }
